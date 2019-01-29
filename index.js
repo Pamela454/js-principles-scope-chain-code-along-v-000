b51c36f42f3347900f3b1cfa22a81d1f8d52fef9
Pamela Frieze <pfrieze@gmail.com>
2019-01-29 23:11:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,11 +17,11 @@ function changeLeastFavoriteCustomer(){
   leastFavoriteCustomer = 'marie';
 }
 
-function congratulateCustomer(){
+function congratulateCustomer() {
   return `congrats ${favoriteCustomer}`
 }
 
-function attemptTwoFavoriteCustomers(){
+function attemptTwoFavoriteCustomers() {
     let favoriteCustomer = 'bar';
     let favoriteCustomer = 'not bar';
 }
